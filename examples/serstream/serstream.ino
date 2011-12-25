7197d27e805f847e3e7b714298b39558e63fe9ea
maniacbug <maniacbug@ymail.com>
2011-12-24 22:40:53 -0800

    Update serstream to include a progmem example

--- examples/serstream/serstream.ino
+++ examples/serstream/serstream.ino
@@ -1,5 +1,6 @@
 #include <StandardCplusplus.h>
 #include <serstream>
+#include <avr/pgmspace.h>
 
 using namespace std;
 
@@ -10,6 +11,9 @@ void setup(void)
   Serial.begin(57600);
 
   cout << "Hello, world." << endl;
+
+  cout << PSTR("I use up absolutely no RAM space whatsoever") << endl;
+  cout << PSTR("so you can use as many PSTR strings as you want!") << endl;
 }
 
 void loop(void)
